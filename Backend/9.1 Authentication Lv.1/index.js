20bd99fe4ee206ce3322e21889df8299024d9413
Ashforfian <yusufkerem2607@gmail.com>
2026-02-10 15:29:22 +0300

    9.1 was appended.

---
+++ Backend/9.1 Authentication Lv.1/index.js
@@ -0,0 +1,71 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  password: "Sifre.",
+  host: "localhost",
+  port: 5432,
+  database: "secrets"
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  
+  try{
+    const result = await db.query("INSERT INTO users (mail,password) VALUES($1, $2) RETURNING *; ",[email, password]);
+    console.log(result);
+    res.render("secrets.ejs");
+  }catch(err){
+    console.log(err);
+    res.redirect("/register");
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try{
+    const result = await db.query("SELECT password FROM users WHERE mail = $1 ",[email]);
+    if(result.rowCount > 0){
+      if(password === result.rows[0].password){
+        res.render("secrets.ejs");
+      }else{
+        console.log("Hatalı giriş");
+        res.redirect("/login");
+      }
+    }else{
+      console.log("Kullanici bulunamadi!");
+      res.redirect("/login");
+    }
+  }catch(err){
+    console.log(err);
+    res.redirect("/login");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
